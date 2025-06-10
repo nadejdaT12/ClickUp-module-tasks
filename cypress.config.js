b1260edccead1d2c55d06d37dc8fe19eaade9509
nadejdaT12 <nadejda.taigunova@gmail.com>
2025-06-10 09:02:43 +0300

    Add config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,29 +1,10 @@
-const { defineConfig } = require('cypress')
-const {allureCypress} = require("allure-cypress/reporter")
-const {platform, release, version} = require("node:os");
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: 'mochawesome',
   e2e: {
     setupNodeEvents(on, config) {
-      allureCypress(on, config, {
-        environmentInfo: {
-          os_platform: platform(),
-          os_release: release(),
-          os_version: version(),
-          node_version: process.version,
-        },
-        resultsDir: "allure-results",
-      })
-      return config;
+      // implement node event listeners here
     },
-    // Configure your E2E tests here
-    specPattern: "cypress/e2e/**/*.{cy,spec}.{js,ts}",
-    baseUrl: "https://api.clickup.com/api/v2",
-    env: {
-      allureLogCypress:false,
-      allureReuseAfterSpec: true,
-      token: 'pk_200589300_JCKLZL75NT02BXJ5IT3H76WVZJO0Q892'
-    }
+    baseUrl: "https://api.clickup.com/api/v2/"
   },
-})
+});
